cd579069d6e90c62f5df0490c4e2991220de6127
Sami Kukkonen <sami@samikukkonen.fi>
2024-10-06 15:53:26 +0300

    Save VBMC experiment results to CSV

--- vbmc_experiment.py
+++ vbmc_experiment.py
@@ -7,11 +7,13 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 import jax
 jax.config.update('jax_enable_x64', True)
 
+import math
 import logging
+import sys
+import time
 from argparse import ArgumentParser
-from contextlib import redirect_stdout
-from dataclasses import dataclass
-from functools import partial
+from datetime import timedelta
+from dataclasses import dataclass, asdict
 from typing import Any
 from pathlib import Path
 from zlib import crc32
@@ -20,13 +22,13 @@ import numpy as np
 import pandas as pd
 import dask.distributed as dd
 import jax.numpy as jnp
-import tensorflow_probability.substrates.jax as tfp
 from jaxtyping import PRNGKeyArray
+from numpy.typing import NDArray
 from pyemd import emd_samples
 from pyvbmc import VBMC, VariationalPosterior
 from pyvbmc.feasibility_estimation import FeasibilityEstimator, OracleFeasibilityEstimator
 from pyvbmc.feasibility_estimation.gpc2 import GPCFeasibilityEstimator
-from toolbox import dprint, wprint, iprint, eprint, Timer
+from toolbox import dprint, wprint, iprint, Timer
 
 from bench.vbmc.tasks import VBMCModel
 from bench.vbmc.tasks.rosenbrock import Rosenbrock
@@ -98,20 +100,47 @@ class Options:
 
     def __post_init__(self):
         assert self.trials > 0, ValueError("invalid trial count")
-        assert self.mcmc_chains > 0, ValueError("invalid trial count")
-        assert self.mcmc_sample_count > 0, ValueError("invalid trial count")
         assert self.vp_sample_count > 0, ValueError("invalid trial count")
 
 type VBMCOptions = dict[str, Any]
 
-def sample_uniform(model, *, seed, sample_shape = ()):
-    xmin, xmax = jnp.unstack(model.plausible_bounds)
-    return jax.random.uniform(seed, sample_shape + (jnp.size(xmin),), minval=xmin, maxval=xmax)
+@dataclass(kw_only=True)
+class VBMCInferenceResult:
+    vp: VariationalPosterior
+    seed: int
+    message: str
+    runtime: timedelta
+    iterations: int
+    target_evaluations: int
+    success: bool
+    reliability_index: float
+    elbo: float
+    elbo_sd: float
+
+@dataclass(kw_only=True)
+class VBMCTrialResult:
+    # region Identifiers
+    experiment: str
+    seed: int
+    # endregion
+
+    # region Configuration
+    vp_sample_checksum: int
+    vp_sample_count: int
+    reference_sample_checksum: int
+    reference_sample_count: int
+    # endregion
+
+    # region Outcomes
+    success: bool
+    emd: float
+    inference_runtime: float
+    # endregion
 
 def get_reference_posterior(model: VBMCModel, *, options: Options, constrained = False):
     return np.load(POSTERIORS_PATH / Path(model.name).with_suffix(".npy"))
 
-def run_vbmc(model: VBMCModel, *, vbmc_options: VBMCOptions = dict(), key: PRNGKeyArray):
+def run_vbmc(model: VBMCModel, key: PRNGKeyArray, *, vbmc_options: VBMCOptions = dict()):
     seed = jax.random.bits(key, dtype=jnp.uint32).item()
 
     vbmc_options.update(display='off')
@@ -132,14 +161,47 @@ def run_vbmc(model: VBMCModel, *, vbmc_options: VBMCOptions = dict(), key: PRNGK
     timer = Timer()
     vp, results = vbmc.optimize()
     elapsed = timer.elapsed
-    dprint(results['message'])
-    if not results['success_flag']:
+
+
+    return VBMCInferenceResult(
+        vp=vp,
+        seed=seed,
+        message=results['message'],
+        runtime=elapsed,
+        iterations=results['iterations'],
+        target_evaluations=results['func_count'],
+        success=results['success_flag'],
+        reliability_index=results['r_index'],
+        elbo=results['elbo'],
+        elbo_sd=results['elbo_sd']
+    )
+
+def run_trial(model: VBMCModel, key: PRNGKeyArray, *, reference_sample: NDArray, options: Options) -> VBMCTrialResult:
+    inference_result = run_vbmc(model, vbmc_options=dict(max_fun_evals=100), key=key)
+    dprint(inference_result.message)
+    if not inference_result.success:
         wprint("VBMC inference did not converge to a stable solution.")
     else:
-        iprint(f"Inference completed in {elapsed}")
-        dprint(f"ELBO: {results['elbo']:.6f} ± {results['elbo_sd']:.6f}")
+        iprint(f"Inference completed in {inference_result.runtime}")
+        dprint(f"ELBO: {inference_result.elbo:.6f} ± {inference_result.elbo_sd:.6f}")
 
-    return vp
+    dprint(f"Generating {options.vp_sample_count} samples from variational posterior")
+    vp_samples, _ = inference_result.vp.sample(options.vp_sample_count)
+    dprint(f"Sample checksum: {crc32(vp_samples)}")
+    emd = emd_samples(reference_sample, vp_samples)
+    iprint(f"EMD: {emd}")
+
+    return VBMCTrialResult(
+        experiment=model.name,
+        seed=inference_result.seed,
+        vp_sample_checksum=crc32(vp_samples),
+        vp_sample_count=options.vp_sample_count,
+        reference_sample_checksum=crc32(reference_sample),
+        reference_sample_count=np.size(reference_sample, 0),
+        emd=emd,
+        success=inference_result.success,
+        inference_runtime=inference_result.runtime.total_seconds()
+    )
 
 def _suppress_noise():
     logging.disable()
@@ -152,15 +214,26 @@ def main():
         _suppress_noise()
 
     model = Rosenbrock()
+    reference_posterior = get_reference_posterior(model, options=options)
+    dprint(f"Loaded reference posterior, sample checksum: {crc32(reference_posterior)}")
+
     key = jax.random.key(options.seed)
-    reference_samples = get_reference_posterior(model, options=options)
-    dprint(f"Loaded reference posterior, sample checksum: {crc32(reference_samples)}")
-    vp = run_vbmc(model, vbmc_options=dict(max_fun_evals=100), key=key)
-    dprint(f"Generating {options.vp_sample_count} samples from variational posterior")
-    vp_samples, _ = vp.sample(options.vp_sample_count)
-    dprint(f"Sample checksum: {crc32(vp_samples)}")
-    emd = emd_samples(reference_samples, vp_samples)
-    iprint(f"EMD: {emd}")
+    experiment_results = []
+
+    experiment_results.append(
+        run_trial(model, key, reference_sample=reference_posterior, options=options)
+    )
+
+    dataframe = pd.DataFrame(map(asdict, experiment_results))
+    dataframe = dataframe.set_index("experiment")
+    timestamp = str(math.trunc(time.time()))
+
+    filename = f"vbmc-experiments-{timestamp}.csv"
+    iprint(f"Saving results to {filename}")
+    dataframe.to_csv(filename)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
