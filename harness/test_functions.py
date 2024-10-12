67feafd0eede2a467ab2f86d97ffc68415d16383
Sami Kukkonen <sami@samikukkonen.fi>
2024-10-12 19:23:56 +0300

    Add Himmelblau function to harness.test_functions

--- harness/test_functions.py
+++ harness/test_functions.py
@@ -2,4 +2,9 @@ import jax.numpy as jnp
 from jaxtyping import Float, Array
 
 def rosen(x: Float[Array, "dim"], *, a = 1., b = 100.) -> float:
-    return jnp.sum(b * jnp.square(x[1:] - jnp.square(x[:-1])) + jnp.square(a - x[:-1]))
+    return jnp.sum(b * (x[1:] - x[:-1] ** 2) ** 2 + (a - x[:-1]) ** 2)
+
+def himmelblau(x: Float[Array, "2"]):
+    return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
+
+__all__ = ["rosen", "himmelblau"]
