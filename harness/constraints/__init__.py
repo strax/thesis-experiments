6a1d5b497a89c5ea189b4619816be1e75cb61522
Sami Kukkonen <sami@samikukkonen.fi>
2024-10-13 15:51:52 +0300

    Move `sinusoid` to `harness.test_functions`

--- harness/constraints/__init__.py
+++ harness/constraints/__init__.py
@@ -1,15 +1,13 @@
 import jax.numpy as jnp
 from jax.nn import sigmoid
 
+from harness.test_functions import sinusoid
+
 from .constraint import Constraint
 from .box import BoxConstraint
 
-def _sinusoid(x, y):
-    return jnp.sin(10 * x) + jnp.cos(8 * y) - jnp.cos(6 * x * y)
-
 def sigmoid_sinusoid_th(theta, *, threshold = 0.55):
-    x, y = jnp.unstack(theta)
-    return sigmoid(_sinusoid(x, y)) <= threshold
+    return sigmoid(sinusoid(theta)) <= threshold
 sigmoid_sinusoid_th.name = sigmoid_sinusoid_th.__name__
 
 def cubicline(theta):
--- harness/test_functions.py
+++ harness/test_functions.py
@@ -7,4 +7,7 @@ def rosen(x: Float[Array, "dim"], *, a = 1., b = 100.) -> float:
 def himmelblau(x: Float[Array, "2"]) -> float:
     return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
 
-__all__ = ["rosen", "himmelblau"]
+def sinusoid(x: Float[Array, "2"]) -> Array:
+    return jnp.sin(10 * x[0]) + jnp.cos(8 * x[1]) - jnp.cos(6 * x[0] * x[1])
+
+__all__ = ["rosen", "himmelblau", "sinusoid"]
