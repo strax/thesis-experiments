1aa86280ddce08a8436796f5987aa3703aee8cf5
Sami Kukkonen <sami@samikukkonen.fi>
2024-10-21 00:25:31 +0300

    Add smap utility function

--- harness/utils.py
+++ harness/utils.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from functools import wraps
+from typing import cast, Callable
+
+import jax
 
 def maybe[T, U](func: Callable[[T], U], value: T | None, default: U) -> U:
     if value is None:
         return default
     return func(value)
+
+def smap[F: Callable](fun: F) -> F:
+    """
+    Sequential map. Creates a function that maps `fun` over argument axes.
+
+    This has the same semantics as `jax.vmap` but applies `fun` sequentially.
+    """
+    @wraps(fun)
+    def wrapper(*args, **kwargs):
+        return jax.lax.map(lambda args: fun(*args, **kwargs), args)
+
+    return cast(F, wrapper)
